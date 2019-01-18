2e19ca0c39c9d4e1b64975afb47cadf46e6c7778
robcarrera <46800254+robcarrera@users.noreply.github.com>
2019-01-17 18:22:58 -0600

    Add files via upload

---
+++ SuperDuperLibrary.framework/Headers/SuperDuperLibrary.h
@@ -0,0 +1,19 @@
+//
+//  SuperDuperLibrary.h
+//  SuperDuperLibrary
+//
+//  Created by Roberto Carrera on 1/17/19.
+//  Copyright © 2019 MuukTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperDuperLibrary.
+FOUNDATION_EXPORT double SuperDuperLibraryVersionNumber;
+
+//! Project version string for SuperDuperLibrary.
+FOUNDATION_EXPORT const unsigned char SuperDuperLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperDuperLibrary/PublicHeader.h>
+
+
